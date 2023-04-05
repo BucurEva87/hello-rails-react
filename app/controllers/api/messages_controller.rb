7618edadcb64b90e0789211390afa1991b88eb59
BucurEva87 <bucur.eva87@gmail.com>
2023-04-05 21:59:08 +0300

    Add App component to root path view

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,10 +1,6 @@
 class Api::MessagesController < ApplicationController
-  def default_route
-    @place = 'A house in New Orleans'
-  end
-
   def random_greeting
-    message = Message.order("RANDOM()").first
+    message = Message.all.sample
     render json: { greeting: message.content }
   end
 end
